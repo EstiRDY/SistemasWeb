b1cc0a8748bc519f57f3100abf972ef94d54dcd8
Esti <erodriguezdeyur001@ikasle.ehu.es>
2017-04-21 21:49:10 +0200

    Se pueden realizar búsquedas pero no se muestra el resultado

--- js_comun.js
+++ js_comun.js
@@ -1,6 +1,11 @@
 
 $('document').ready(function(){
 
-document.getElementById('navbar').innerHTML = "<nav>NAAAV</nav>";
+document.getElementById('navbar').innerHTML = "<nav>"
+											+"<div class='nav-wrapper'>"
+											+"<a href='login.html'>Iniciar sesión</a>"
+											+"<a href='busqueda.html'><i class='material-icons'>search</i></a>"
+											+"</div>"
+											+"</nav>";
 
 });
